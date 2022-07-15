130bc311099b3e90784106b2d3dfc9711c479363
Binoj Daniel <binojdaniel@gmail.com>
2022-07-15 14:55:42 -0400

    This uses finviz financial package to get data

---
+++ float-scraper-finviz.py
@@ -0,0 +1,22 @@
+from time import time
+from finvizfinance.quote import finvizfinance
+
+# Prompt for ticker symbol (e.g. AAPL)
+stock = input('Enter ticker symbol:')
+
+# Set start time to measure the speed of the whole operation
+start = time()
+
+# Get float from shortsqueeze.com
+#try:
+stock_data = finvizfinance(stock)
+stock_fundament = stock_data.ticker_fundament()
+print('Shares Float: ' + stock_fundament['Shs Float'])
+
+# If not available, print error message
+# except:
+#     print('No data available')
+    
+# Calculate the time taken for the entire scraping operation
+end = time()
+print('Time taken: ' + str(round(end - start, 2)) + 's')
